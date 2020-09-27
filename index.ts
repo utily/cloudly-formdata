287a03c605d5f6115f0eaf9d8391bde9ede2b910
Simon Mika <simon@mika.se>
2020-09-27 16:35:17 +0200

    Added missing exports.

--- index.ts
+++ index.ts
@@ -1,1 +1,6 @@
-export {}
+import { Blob } from "./Blob"
+import { File } from "./File"
+import { FormData } from "./FormData"
+import { FormDataEntryValue } from "./FormDataEntryValue"
+
+export { Blob, File, FormData, FormDataEntryValue }
